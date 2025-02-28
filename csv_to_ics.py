db2d8ee2998dd55291cfdc8d37869637ad6460c0
doc.void <81415427+docVoid@users.noreply.github.com>
2025-02-28 11:07:26 +0100

    changed log language to english (#8)

--- csv_to_ics.py
+++ csv_to_ics.py
@@ -15,17 +15,17 @@ def csv_to_ics(csv_file, ics_file):
                 try:
                     date = datetime.strptime(birthday, '%Y-%m-%d').date()
                     event = Event()
-                    event.add('summary', f'Geburtstag: {name.strip()}')
+                    event.add('summary', f'Birthday: {name.strip()}')
                     event.add('dtstart', date)
                     event.add('rrule', {'freq': 'yearly'})
                     cal.add_component(event)
                 except ValueError:
-                    print(f'Ungültiges Datum für {name}: {birthday}')
+                    print(f'Invalid date for {name}: {birthday}')
     
     with open(ics_file, 'wb') as f:
         f.write(cal.to_ical())
     
-    print(f'ICS-Datei gespeichert als {ics_file}')
+    print(f'ICS-File saved as {ics_file}')
 
 if __name__ == "__main__":
     csv_to_ics('contacts.csv', 'birthdays.ics') # Change to your filepath
